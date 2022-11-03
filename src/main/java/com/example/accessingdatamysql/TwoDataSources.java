0de728a9063b61f4a4cc716c1c507e88bff19b51
padmahasa <padmahas@gmail.com>
2022-11-03 11:44:13 +0530

    Fixed issue of DataSource being reinitialized to Null.
    1. New object for "DataSourceProperties" shouldn't have been created
    manually, "DataSourceProperties firstDataSourceProps =
    firstDataSourceProperties();". Hence removed object initialization for
    both the database and instead injected the "DataSourceProperties".

--- src/main/java/com/example/accessingdatamysql/TwoDataSources.java
+++ src/main/java/com/example/accessingdatamysql/TwoDataSources.java
@@ -17,64 +17,67 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration(proxyBeanMethods = false)
+@EnableJpaRepositories
 public class TwoDataSources {
 
-    @Bean
-    @Primary
-    @ConfigurationProperties("app.datasource.first")
-    public DataSourceProperties firstDataSourceProperties() {
-        return new DataSourceProperties();
-    }
+	@Bean
+	@Primary
+	@ConfigurationProperties("app.datasource.first")
+	public DataSourceProperties firstDataSourceProperties() {
+		return new DataSourceProperties();
+	}
 
-    @Bean
-    @Primary
-    @ConfigurationProperties("app.datasource.first.configuration")
-    public HikariDataSource firstDataSource(DataSourceProperties firstDataSourceProperties) {
-        return firstDataSourceProperties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
-    }
-    
-    @Bean
-    public LocalContainerEntityManagerFactoryBean firstEntityManagerFactory(DataSource firstDataSource) {
-    	HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+	@Bean
+	@Primary
+	@ConfigurationProperties("app.datasource.first.configuration")
+	public HikariDataSource firstDataSource(DataSourceProperties firstDataSourceProperties) {
+		return firstDataSourceProperties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
+	}
+
+	@Bean
+	@Primary
+	public LocalContainerEntityManagerFactoryBean firstEntityManagerFactory(DataSource firstDataSource,
+			DataSourceProperties firstDataSourceProps) {
+		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
 
 		LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
-		DataSourceProperties firstDataSourceProps = firstDataSourceProperties();
 		factoryBean.setDataSource(firstDataSource(firstDataSourceProps));
 		factoryBean.setJpaVendorAdapter(vendorAdapter);
 		factoryBean.setPackagesToScan(TwoDataSources.class.getPackage().getName());
 
 		return factoryBean;
-    }
+	}
 
-    @Bean
-    @ConfigurationProperties("app.datasource.second")
-    public DataSourceProperties secondDataSourceProperties() {
-        return new DataSourceProperties();
-    }
+	@Bean
+	@ConfigurationProperties("app.datasource.second")
+	public DataSourceProperties secondDataSourceProperties() {
+		return new DataSourceProperties();
+	}
 
-    @Bean
-    @ConfigurationProperties("app.datasource.second.configuration")
-    public HikariDataSource secondDataSource(
-            @Qualifier("secondDataSourceProperties") DataSourceProperties secondDataSourceProperties) {
-        return secondDataSourceProperties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
-    }
-    
-    @Bean
-    public LocalContainerEntityManagerFactoryBean secondEntityManagerFactory(DataSource secondDataSource) {
-    	HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+	@Bean
+	@ConfigurationProperties("app.datasource.second.configuration")
+	public HikariDataSource secondDataSource(
+			@Qualifier("secondDataSourceProperties") DataSourceProperties secondDataSourceProperties) {
+		return secondDataSourceProperties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
+	}
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean secondEntityManagerFactory(DataSource secondDataSource,
+			DataSourceProperties secondDataSourceProps) {
+		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
 		vendorAdapter.setGenerateDdl(true);
 
 		LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
-		DataSourceProperties secondDataSourceProps = secondDataSourceProperties();
-		factoryBean.setDataSource(firstDataSource(secondDataSourceProps));
+		factoryBean.setDataSource(secondDataSource(secondDataSourceProps));
 		factoryBean.setJpaVendorAdapter(vendorAdapter);
 		factoryBean.setPackagesToScan(TwoDataSources.class.getPackage().getName());
 
 		return factoryBean;
-    }
+	}
 
 }
